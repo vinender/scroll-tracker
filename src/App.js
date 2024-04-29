edbb66a426a9fb994c829e39b82f3e762db08896
vinender <vinendersingh91@gmail.com>
2024-04-29 12:54:47 +0530

    update

--- src/App.js
+++ src/App.js
@@ -2,12 +2,11 @@ import React from 'react';
  import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ScrollTracker from './components/scroll-tracker';
 import RenderButton from './components/render-button';
-import ScrollTracker from './components/scroll-tracker';
-
+ 
 function App() {
   return (
     <Routes>
-      // <Route path="/" element={<RenderButton />} />
+       <Route path="/quiz" element={<RenderButton />} />
 <Route path="/" element={<ScrollTracker />} />
   
     </Routes>
