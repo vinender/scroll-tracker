300c4fe2cb4bb80e1781eea558fd2fdd7d03fd40
vinender singh <46550815+vinender@users.noreply.github.com>
2024-04-29 12:48:28 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
  import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ScrollTracker from './components/scroll-tracker';
 import RenderButton from './components/render-button';
+import ScrollTracker from './components/scroll-tracker';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<RenderButton />} />
+      // <Route path="/" element={<RenderButton />} />
+<Route path="/" element={<ScrollTracker />} />
+  
     </Routes>
   );
 }
